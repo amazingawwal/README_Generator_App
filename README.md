cb92a4c9708f22b8fdd03085a875a8acada16551
Awwal <sakeebmawwal@gmail.com>
2023-12-17 01:25:44 +0000

    added files and genrated markdown ftn

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# README_Generator_App
-This app automatically generates a README Doc based on users input in a few steps and a very short time
+# Title
+
+## License
+mit
+
+## Description
+description
+
+## Table of Content
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license-1)
+    - [Contributing](#contributing)
+    - [Tests](#test)
+    - [Questions](#questions)
+
+## Installation
+To install necessary dependencies, run the following
+
+```npm 1```
+
+## Usage
+To use, master Command Line Interface for Windows users or Terminal for Mac users.
+
+## License
+The project is under MIT license
+
+### Contributing
+
+call me
+
+### Test 
+To run the test, use the following command
+
+```npm test```
+
+### Questions
+
+For question about the repo, reach me at email, for work check out my github
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,47 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `
+    # ${data.title}
+    ## License
+    ${data.license}
+
+    ## Description
+    ${data.description}
+
+    ## Table of Content
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license-1)
+        - [Contributing](#contributing)
+        - [Tests](#test)
+        - [Questions](#questions)
+
+    ## Installation
+    To install necessary dependencies, run the following
+
+    ${data.install}
+
+    ## Usage
+    ${data.usage}
+
+    ## License
+    The project is under ${data.license} license
+
+    ### Contributing
+
+    ${data.contribution}
+
+    ### Test 
+    To run the test, use the following command
+
+    ${data.test}
+
+    ### Questions
+
+    For question about the repo, reach me at ${data.email}, for work check out my ${data.github} 
+
+
+`;
+}
+
+module.exports = generateMarkdown;
